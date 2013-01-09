6182be8c4e164a2ffd00a0c67c9e2b57942ccde7
glenwatson <glen.watson@alumni.neumont.edu>
2013-01-09 10:17:03 -0500

    Class level comments for 1st & 2nd class members

--- src/watson/glen/pseudocode/constructs/FirstClassMember.java
+++ src/watson/glen/pseudocode/constructs/FirstClassMember.java
@@ -1,5 +1,9 @@
 package watson.glen.pseudocode.constructs;
-
+/**
+ * Representation of a First-class citizen
+ * @author glen.watson
+ *
+ */
 public class FirstClassMember extends LanguageConstruct
 {
 	public FirstClassMember(String name)
--- src/watson/glen/pseudocode/constructs/SecondClassMember.java
+++ src/watson/glen/pseudocode/constructs/SecondClassMember.java
@@ -1,5 +1,10 @@
 package watson.glen.pseudocode.constructs;
-
+/**
+ * Representation of a second-class citizen, i.e. top-level members in first-class citizens.
+ * e.g. instance variables and methods in classes & method signatures in interfaces
+ * @author glen.watson
+ *
+ */
 public class SecondClassMember extends LanguageConstruct
 {
 	public SecondClassMember(String name)
