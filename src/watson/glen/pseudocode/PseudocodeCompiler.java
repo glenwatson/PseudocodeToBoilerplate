1df4ce565e2866a47d0aeb151813791cca01047a
glenwatson <glen.watson@alumni.neumont.edu>
2013-01-27 15:29:06 -0500

    Consolidated Tokenizer & LineTokenizer into Tokenizer

--- src/watson/glen/pseudocode/PseudocodeCompiler.java
+++ src/watson/glen/pseudocode/PseudocodeCompiler.java
@@ -2,15 +2,12 @@ package watson.glen.pseudocode;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 
 import watson.glen.pseudocode.constructs.LanguageConstruct;
 import watson.glen.pseudocode.interpreter.Interpreter;
 import watson.glen.pseudocode.languagewriter.FileSystemWriter;
 import watson.glen.pseudocode.tokenizer.LineToken;
-import watson.glen.pseudocode.tokenizer.LineTokenizer;
 import watson.glen.pseudocode.tokenizer.Tokenizer;
 
 /**
--- src/watson/glen/pseudocode/tokenizer/Tokenizer.java
+++ src/watson/glen/pseudocode/tokenizer/Tokenizer.java
@@ -8,6 +8,8 @@ import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -16,6 +18,12 @@ import java.util.Scanner;
  */
 public class Tokenizer
 {
+	private static String tab = "\\t";
+	private static String wordOrName = "[a-zA-Z][\\w\\d]+";
+	private static String comments = "//|/\\*|\\*/";
+	private static String specialCharacters = "[^\\s]+?";
+	private static Pattern tokenizePattern = Pattern.compile(tab+"|"+comments+"|"+wordOrName+"|"+specialCharacters);
+	
 	/**
 	 * Parses the stream into tokens
 	 * @param inStream the data to tokenize
@@ -30,10 +38,34 @@ public class Tokenizer
 			{
 				String line = scan.nextLine();
 				System.out.println("read line: "+line);
-				LineToken lineToken = LineTokenizer.tokenize(line);
+				LineToken lineToken = Tokenizer.tokenizeLine(line);
 				lineTokens.add(lineToken);
 			}
 		}
 		return lineTokens;
+	}
+
+	private static LineToken tokenizeLine(String line)
+	{
+		Matcher matcher = tokenizePattern.matcher(line);
+		
+		List<Token> tokens = new LinkedList<Token>();
+		boolean lineComment = false;
+		
+		while(matcher.find() && !lineComment)
+		{
+			String group = matcher.group();
+			if(group.equals("//"))
+			{
+				group = line.substring(matcher.start());
+				lineComment = true;
+			}
+			Token token = new Token(group);
+			tokens.add(token);
+		}
+		
+		LineToken lineToken = new LineToken();
+		lineToken.setTokens(tokens);
+		return lineToken;
 	}
 }
