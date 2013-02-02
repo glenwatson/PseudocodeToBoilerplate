7a4b520b27aa3e1a7bedbc64faa1414af98260e4
glenwatson <glen.watson@alumni.neumont.edu>
2013-02-02 02:08:26 -0500

    added interface methods for each language construct

--- src/watson/glen/pseudocode/codegenerator/CSharpGenerator.java
+++ src/watson/glen/pseudocode/codegenerator/CSharpGenerator.java
@@ -1,12 +1,76 @@
 package watson.glen.pseudocode.codegenerator;
 
-import watson.glen.pseudocode.constructs.LanguageConstruct;
+import watson.glen.pseudocode.constructs.AccessModifier;
+import watson.glen.pseudocode.constructs.ClassConstruct;
+import watson.glen.pseudocode.constructs.EnumConstruct;
+import watson.glen.pseudocode.constructs.InstanceVariable;
+import watson.glen.pseudocode.constructs.InterfaceConstruct;
+import watson.glen.pseudocode.constructs.Method;
+import watson.glen.pseudocode.constructs.MethodLine;
+import watson.glen.pseudocode.constructs.MethodSignature;
+import watson.glen.pseudocode.constructs.Type;
+import watson.glen.pseudocode.constructs.VariableDeclaration;
+
 
 public class CSharpGenerator implements CodeGeneratable
 {
-	
+
 	@Override
-	public CodeRepresentation generate(LanguageConstruct construct)
+	public CodeRepresentation generate(EnumConstruct enumConstruct)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(InstanceVariable instanceVariable)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(ClassConstruct cClassConstruct)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(Method method)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(InterfaceConstruct interfaceConstruct)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(VariableDeclaration variableDeclaration)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(Type type)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(AccessModifier accessModifier)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(MethodLine methodLine)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(MethodSignature methodSignature)
 	{
 		throw new RuntimeException("Not implemented");
 	}
