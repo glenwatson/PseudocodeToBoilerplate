ca0b9c3c74858d5321ab3e25fd60b5e87bf454d9
glenwatson <glen.watson@alumni.neumont.edu>
2013-02-02 02:08:26 -0500

    added interface methods for each language construct

--- src/watson/glen/pseudocode/codegenerator/CSharpGenerator.java
+++ src/watson/glen/pseudocode/codegenerator/CSharpGenerator.java
@@ -1,6 +1,17 @@
 package watson.glen.pseudocode.codegenerator;
 
+import watson.glen.pseudocode.constructs.AccessModifier;
+import watson.glen.pseudocode.constructs.ClassConstruct;
+import watson.glen.pseudocode.constructs.EnumConstruct;
+import watson.glen.pseudocode.constructs.InstanceVariable;
+import watson.glen.pseudocode.constructs.InterfaceConstruct;
 import watson.glen.pseudocode.constructs.LanguageConstruct;
+import watson.glen.pseudocode.constructs.Method;
+import watson.glen.pseudocode.constructs.MethodLine;
+import watson.glen.pseudocode.constructs.MethodSignature;
+import watson.glen.pseudocode.constructs.Type;
+import watson.glen.pseudocode.constructs.VariableDeclaration;
+
 
 public class CSharpGenerator implements CodeGeneratable
 {
@@ -10,5 +21,65 @@ public class CSharpGenerator implements CodeGeneratable
 	{
 		throw new RuntimeException("Not implemented");
 	}
-	
+
+	@Override
+	public CodeRepresentation generate(EnumConstruct enumConstruct)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(InstanceVariable instanceVariable)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(ClassConstruct cClassConstruct)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(Method method)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(InterfaceConstruct interfaceConstruct)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(VariableDeclaration variableDeclaration)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(Type type)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(AccessModifier accessModifier)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(MethodLine methodLine)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public CodeRepresentation generate(MethodSignature methodSignature)
+	{
+		throw new RuntimeException("Not implemented");
+	}
+
 }
--- src/watson/glen/pseudocode/codegenerator/CodeGeneratable.java
+++ src/watson/glen/pseudocode/codegenerator/CodeGeneratable.java
@@ -1,8 +1,28 @@
 package watson.glen.pseudocode.codegenerator;
 
+import watson.glen.pseudocode.constructs.AccessModifier;
+import watson.glen.pseudocode.constructs.ClassConstruct;
+import watson.glen.pseudocode.constructs.EnumConstruct;
+import watson.glen.pseudocode.constructs.InstanceVariable;
+import watson.glen.pseudocode.constructs.InterfaceConstruct;
 import watson.glen.pseudocode.constructs.LanguageConstruct;
+import watson.glen.pseudocode.constructs.Method;
+import watson.glen.pseudocode.constructs.MethodLine;
+import watson.glen.pseudocode.constructs.MethodSignature;
+import watson.glen.pseudocode.constructs.Type;
+import watson.glen.pseudocode.constructs.VariableDeclaration;
 
 public interface CodeGeneratable
 {
 	public CodeRepresentation generate(LanguageConstruct construct);
+	public CodeRepresentation generate(EnumConstruct enumConstruct);
+	public CodeRepresentation generate(InstanceVariable instanceVariable);
+	public CodeRepresentation generate(ClassConstruct cClassConstruct);
+	public CodeRepresentation generate(Method method);
+	public CodeRepresentation generate(InterfaceConstruct interfaceConstruct);
+	public CodeRepresentation generate(VariableDeclaration variableDeclaration);
+	public CodeRepresentation generate(Type type);
+	public CodeRepresentation generate(AccessModifier accessModifier);
+	public CodeRepresentation generate(MethodLine methodLine);
+	public CodeRepresentation generate(MethodSignature methodSignature);
 }
