c57a9df1b891a746d11466979062a0570151c4a6
glenwatson <glen.watson@alumni.neumont.edu>
2013-02-16 19:07:23 -0500

    added command line parsing using Apache commons CLI

--- src/watson/glen/pseudocode/PseudocodeCompiler.java
+++ src/watson/glen/pseudocode/PseudocodeCompiler.java
@@ -7,6 +7,14 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
 import watson.glen.pseudocode.codegenerator.CodeGeneratable;
 import watson.glen.pseudocode.codegenerator.CodeRepresentation;
 import watson.glen.pseudocode.codegenerator.opp.JavaGenerator;
@@ -18,36 +26,58 @@ import watson.glen.pseudocode.writer.CodeWriter;
 
 /**
  * @author glen.watson
- *
  */
 public class PseudocodeCompiler
 {
-	
 	/**
 	 * @param args
 	 * @throws FileNotFoundException 
 	 */
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) throws FileNotFoundException
 	{
-		String fileName = "pseudocode.txt";
-		if(args.length >= 1)
-		{
-			fileName = args[0];
-		}
-		else
-		{
-			System.out.println("Please supply a text file to parse");
-		}
-		String outputDirectory = "Output\\";
-		if(args.length >= 2)
-		{
-			outputDirectory = args[1];
-		}
-		else
-		{
-			System.out.println("Please supply a output directory");
-		}
+		//Set up arguments
+		Options options = new Options();
+		options.addOption("v", false, "be verbose");
+		Option i = OptionBuilder.withArgName("file")
+	        .hasArg()
+	        .withDescription("input file. The file containing the pseudocode.")
+	        .create('i');
+		Option o = OptionBuilder.withArgName("folder")
+	        .hasArg()
+	        .withDescription("output folder. The folder to write the output files to.")
+	        .create('o');
+		Option h = OptionBuilder
+		        .withDescription("displays help")
+		        .create("help");
+		options.addOption(i);
+		options.addOption(o);
+		options.addOption(h);
 		
+		try
+		{
+			CommandLine cmd = new PosixParser().parse(options, args);
+			if(cmd.hasOption("help"))
+			{
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.printHelp("PseudocodeCompiler", options);
+			}
+			else
+			{
+				String fileName = cmd.getOptionValue('i');
+				String outputDirectory = cmd.getOptionValue('o');
+				
+				compile(fileName, outputDirectory);
+			}
+			
+		} catch (ParseException e1)
+		{
+			System.err.println("Bad arguments: " + e1.getMessage());
+		}
+	}
+
+	private static void compile(String fileName, String outputDirectory) throws FileNotFoundException
+	{
 		FileInputStream inStream = new FileInputStream(fileName);
 		
 		List<LineToken> lineTokens = Tokenizer.tokenize(inStream);
